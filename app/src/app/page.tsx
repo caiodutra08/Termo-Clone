54cabc151b7fc13c5c6b4eb4613a00eb5efbba09
richard-melo <richard.meloneto@gmail.com>
2023-11-20 21:29:21 -0300

    feat: input wip

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,22 +1,43 @@
 "use client";
 
-import React from "react";
+import React, { KeyboardEvent } from "react";
+import * as ReactDOm from "react-dom";
 
 export default function Home() {
   const [step, setStep] = React.useState();
   const [word, setWord] = React.useState("");
+  const divRef = React.useRef<HTMLDivElement>();
 
   React.useEffect(() => {
     setWord("Dildo");
-    const allIptDivs = document.querySelectorAll("div[data-config='ipt']")
-    allIptDivs.forEach((el) => {
-      el.addEventListener("keydown", (e) => {
-        if (e.keyCode === "39") {
-          
-        }
-      })
-    })
+    const allIptDivs = document.querySelectorAll("div[data-config='ipt']");
+    const editDiv = document.querySelector("edit") as HTMLDivElement;
+    if (editDiv) {
+    }
   }, []);
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === "ArrowRight") {
+      const nextSibling = e.currentTarget?.nextElementSibling as HTMLDivElement;
+      if (nextSibling) {
+        divRef.current = nextSibling;
+        divRef.current.focus();
+      }
+    } else if (e.key === "ArrowLeft") {
+      const previousSibling = e.currentTarget
+        ?.previousSibling as HTMLDivElement;
+      if (previousSibling) {
+        divRef.current = previousSibling;
+        divRef.current.focus();
+      }
+    } else {
+      const nextSibling = e.currentTarget?.nextElementSibling as HTMLDivElement;
+      if (nextSibling) {
+        divRef.current = nextSibling;
+        divRef.current.focus();
+      }
+    }
+  };
 
   return (
     <>
@@ -26,8 +47,18 @@ export default function Home() {
         </header>
         <div className="flex flex-col align-center justify-center w-full">
           <div className="flex flex-row justify-center gap-4">
-            <div className="rounded-md border-2 border-neutral-950 w-12 h-12 text-center bg-white edit" data-config="ipt" />
-            <div className="rounded-md border-2 border-neutral-950 w-12 h-12 text-center bg-white" data-config="ipt"/>
+            <input
+              className="rounded-md border-2 border-neutral-950 w-12 h-12 text-center focus:outline-none bg-white edit"
+              data-config="ipt"
+              onKeyDown={handleKeyDown}
+              maxLength={1}
+            />
+            <input
+              className="rounded-md border-2 border-neutral-950 w-12 h-12 text-center focus:outline-none bg-white"
+              data-config="ipt"
+              onKeyDown={handleKeyDown}
+              maxLength={1}
+            />
           </div>
           <div className="flex"></div>
         </div>
